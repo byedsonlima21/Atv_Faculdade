744e0cfc298f30606afab40249d8a655a9d37992
micro <edsonerione@gmail.com>
2026-09-06 14:24:16 -0300

    Primeira atividade online da faculdade feita

--- AtvUnopar.c
+++ AtvUnopar.c
@@ -2,21 +2,67 @@
 
 int main(){
 
-    int a, b, c;
+    int a, b, c, soma, sub, multi;
+    float div;
 
-    printf("Digite o primeiro valor: ");
+    printf("Digite o primeiro numero: ");
     scanf("%i", &a);
 
-    printf("Digite o segundo valor: ");
+    printf("Digite o segundo numero: ");
     scanf("%i", &b);
 
-    printf("Digite o terceiro valor: ");
+    printf("Digite o terceiro numero: ");
     scanf("%i", &c);
 
-    printf("=+=+=+=+=+ Teste 1 =+=+=+=+=+\nSoma: %d + %d + %d : %d\n", a, b, c, a+b+c);
-    printf("=+=+=+=+=+ Teste 2 =+=+=+=+=+\nSoma: %d - %d - %d : %d\n", a, b, c, a-b-c);
-    printf("=+=+=+=+=+ Teste 3 =+=+=+=+=+\nSoma: %d * %d * %d : %d\n", a, b, c, a*b*c);
-    printf("=+=+=+=+=+ Teste 2 =+=+=+=+=+\nSoma: %d / %d / %d : %d\n", a, b, c, a/b/c);
+    soma = a + b + c;
+    sub = a - b - c;
+    multi = a * b * c;
+
+    printf("\n=+=+=+=+=+ Teste 1 =+=+=+=+=+\nSoma: %d + %d + %d : %d\n", a, b, c, soma);
+    printf("\n=+=+=+=+=+ Teste 2 =+=+=+=+=+\nSubtracao: %d - %d - %d : %d\n", a, b, c, sub);
+    printf("\n=+=+=+=+=+ Teste 3 =+=+=+=+=+\nMultiplicacao: %d * %d * %d : %d\n", a, b, c, multi);
+
+    if(b != 0 && c != 0){
+
+        div = (float)a / b / c;
+        
+        printf("\n=+=+=+=+=+ Teste 4 =+=+=+=+=+\nDivisao: %d / %d / %d : %.2f\n", a, b, c, div);
+    
+    } else {
+    
+        printf("\nDivisao indefinida, tentativa de dividisao por 0");
+
+    }
+
+    if (a > b) {
+
+        printf("\nO primeiro numero es %i portanto ele es maior que o segundo %i", a, b);
+
+    } else {
+
+        printf("\nO segudo numero es %i portanto ele es maior que o primeiro %i", b, a);
+
+    }
+
+    if ( b > c) {
+
+        printf("\nO segundo numero es %i portanto ele es maior que o terceiro %i\n", b, c);
+
+    } else {
+
+        printf("\nO terceiro numero es %i portanto ele es maior que o segudo %i\n", c, b);
+
+    }
+
+    if (a > 0 && b % 2 == 0) {
+
+        printf("\nO primeiro numero es %i es positivo e o segundo %i es par.\n", a, b);
+
+    } else {
+
+        printf("As condicoes logicas nao forma atendidas");
+
+    }
 
     return 0;
 }
